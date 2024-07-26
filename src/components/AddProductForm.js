e3bca52486909fc10dab0e68a92b724d1535fe43
tejosumanth <pavuluritejo@gmail.com>
2024-07-26 18:17:00 +0000

    Finalize AddProductForm.js implementation

--- src/components/AddProductForm.js
+++ src/components/AddProductForm.js
@@ -44,3 +44,94 @@ export default function AddProductForm() {
       description: description.value,
       category_id: parseInt(category.value),
       price: parseFloat(price.value),
+      weight: parseFloat(weight.value),
+      picture1: pictures[0],
+      picture2: pictures[1],
+      picture3: pictures[2]
+    };
+
+    addProduct(product);
+  };
+
+  return (
+    <Modal
+      trigger={
+        <Button primary fluid>
+          Add new Product
+        </Button>
+      }
+    >
+      <Modal.Header>Add new Product</Modal.Header>
+      <Modal.Content>
+        <Form onSubmit={handleSubmit}>
+          <Form.Input
+            name="name"
+            label="Name"
+            placeholder="Product name"
+            onChange={handleChange1}
+            value={name.value}
+          />
+          <Form.Input
+            name="description"
+            label="Description"
+            placeholder="Product description (Optional)"
+            onChange={handleChange2}
+            value={description.value}
+          />
+          <Form.Field>
+            <Header as="h5">Category</Header>
+            <Dropdown
+              name="category"
+              placeholder="Category"
+              fluid
+              selection
+              options={listCategories}
+              onChange={handleChange3}
+              value={category.value}
+            />
+          </Form.Field>
+          <Form.Group widths="equal">
+            <Form.Input
+              name="price"
+              label="Price"
+              placeholder="Price (USD)"
+              onChange={handleChange4}
+              value={price.value}
+            />
+            <Form.Input
+              name="weight"
+              label="Weight"
+              placeholder="Weigth (Kg)"
+              onChange={handleChange5}
+              value={weight.value}
+            />
+          </Form.Group>
+          <Form.Group widths="equal">
+            <Form.Input
+              name="picture1"
+              label="Image 1"
+              placeholder="Image 1 URL"
+              onChange={handleChange6}
+              value={picture1.value}
+            />
+            <Form.Input
+              name="picture2"
+              label="Image 2"
+              placeholder="Image 2 URL"
+              onChange={handleChange7}
+              value={picture2.value}
+            />
+            <Form.Input
+              name="picture3"
+              label="Image 3"
+              placeholder="Image 3 URL"
+              onChange={handleChange8}
+              value={picture3.value}
+            />
+          </Form.Group>
+          <Button type="submit">Add</Button>
+        </Form>
+      </Modal.Content>
+    </Modal>
+  );
+}
