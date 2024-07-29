d70692875987d9daf49fed7db7788b6255f0f45c
tejosumanth <pavuluritejo@gmail.com>
2024-07-29 18:59:00 +0000

    Finalize connector.js implementation

--- src/config/connector.js
+++ src/config/connector.js
@@ -39,3 +39,85 @@ const Context = props => {
       type: GET_CURRENCY,
       payload: res.data
     });
+  };
+
+  // POST Methods
+  const addUser = async user => {
+    const res = await axiosClient.post("/users/add", user);
+
+    localStorage.setItem("user", JSON.stringify(res.data));
+
+    dispatch({
+      type: POST_USER,
+      payload: res.data
+    });
+  };
+  const addCategory = async category => {
+    const res = await axiosClient.post("/categories/add", category);
+
+    dispatch({
+      type: POST_CATEGORY,
+      payload: res.data
+    });
+  };
+  const addProduct = async product => {
+    const res = await axiosClient.post("/products/add", product);
+
+    dispatch({
+      type: POST_PRODUCTS,
+      payload: res.data
+    });
+  };
+
+  // GET Methods
+  const getUser = async user => {
+    const res = await axiosClient.get("/users/" + user.username);
+
+    if (res.data.password === user.password) {
+      localStorage.setItem("user", JSON.stringify(user));
+    }
+
+    dispatch({
+      type: GET_USER,
+      payload: res.data
+    });
+  };
+  const getCategories = async () => {
+    const res = await axiosClient.get("/categories/all");
+
+    dispatch({
+      type: GET_CATEGORIES,
+      payload: res.data
+    });
+  };
+  const getProducts = async () => {
+    const res = await axiosClient.get("/products/all");
+
+    dispatch({
+      type: GET_PRODUCTS,
+      payload: res.data
+    });
+  };
+
+  return (
+    <userContext.Provider
+      value={{
+        user: state.user,
+        currency: state.currency,
+        categories: state.categories,
+        products: state.products,
+        addUser,
+        addCategory,
+        addProduct,
+        getUser,
+        getCategories,
+        getProducts,
+        getCurrency
+      }}
+    >
+      {props.children}
+    </userContext.Provider>
+  );
+};
+
+export default Context;
