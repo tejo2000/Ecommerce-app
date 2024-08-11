b56f46025d25b74f21e8bd174b385a48d25915b4
tejosumanth <pavuluritejo@gmail.com>
2024-08-11 23:01:00 +0000

    Finalize CategoryResource.java implementation

--- src/main/java/com/devrobot/springbootecommerce/resource/CategoryResource.java
+++ src/main/java/com/devrobot/springbootecommerce/resource/CategoryResource.java
@@ -44,3 +44,48 @@ public class CategoryResource {
 	/**
 	 * Gets category.
 	 *
+	 * @return category if exists
+	 */
+	@GetMapping(value = "/get")
+	public Category get(@RequestParam("id") int id) {
+		return categoryRepository.findById(id).get();
+	}
+	/**
+	 * Persist category.
+	 *
+	 * @param category the category
+	 * @return the list
+	 */
+	@PostMapping(value = "/add")
+	public List<Category> persist(@RequestBody final Category category) {
+		categoryRepository.save(category);
+		return categoryRepository.findAll();
+	}
+	/**
+	 * Delete category.
+	 *
+	 * @param id the id
+	 * @return all categories
+	 */
+	@DeleteMapping(value = "/delete")
+	public List<Category> delete(@PathVariable int id) {
+		categoryRepository.deleteById(id);
+		return categoryRepository.findAll();
+	}
+	/**
+	 * Put category.
+	 *
+	 * @param id 		the id
+	 * @param category  the category
+	 * @return all categories
+	 */
+	@PutMapping(value = "/put/{id}")
+	public List<Category> put(@PathVariable int id, @RequestBody Category category) {
+		if (categoryRepository.existsById(id)) {
+			categoryRepository.deleteById(id);
+			categoryRepository.save(category);
+		}
+		
+		return categoryRepository.findAll();
+	}
+}
